0a4ad55ca8e78c292413242de80fe145a56d476b
ehxie <815438732@qq.com>
2023-11-16 10:30:43 +0800

    feat(content-provider): ContactsTest

--- content-provider/ContactsTest/app/src/main/java/com/example/contactstest/MainActivity.kt
+++ content-provider/ContactsTest/app/src/main/java/com/example/contactstest/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.contactstest
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,37 +17,71 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.contactstest.ui.theme.ContactsTestTheme
 
 class MainActivity : ComponentActivity() {
+    private val contactsList = ArrayList<String>()
+    private lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ContactsTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contactsList)
+        val contactsView = findViewById<ListView>(R.id.contactsView)
+        contactsView.adapter = adapter
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CALL_PHONE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), 1)
+        } else {
+            readContacts()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    readContacts()
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_LONG).show()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ContactsTestTheme {
-        Greeting("Android")
+    @SuppressLint("Range")
+    private fun readContacts() {
+        contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+            ?.apply {
+                while (moveToNext()) {
+                    // 联系人姓名
+                    val displayName =
+                        getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                    // 联系人手机号
+                    val number = getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    contactsList.add("$displayName\n$number")
+                }
+                adapter.notifyDataSetChanged()
+                close()
+            }
     }
 }
